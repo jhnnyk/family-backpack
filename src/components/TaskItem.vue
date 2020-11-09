9d2217a1350c1d6b905326a2963ad7df457e9fc3
John Knotts <jhnnyk@gmail.com>
2020-11-09 13:44:21 -0700

    edit tasks

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="task-item">
-    <input
-      type="checkbox"
-      :name="task.id"
-      :id="task.id"
-      :checked="!!task.completed"
-      @click="toggleTaskComplete(task)"
-    />
-    <label :for="task.id">{{ task.content }}</label>
+    <div v-if="!currentlyEditing">
+      <input
+        type="checkbox"
+        :name="task.id"
+        :id="task.id"
+        :checked="!!task.completed"
+        @click="toggleTaskComplete(task)"
+      />
+      <label :for="task.id">{{ task.content }}</label>
+      <a @click="currentlyEditing = true" title="edit"
+        ><i class="material-icons">edit</i></a
+      >
+    </div>
+
+    <div v-if="currentlyEditing">
+      <input
+        type="text"
+        v-model="task.content"
+        @keydown.enter="saveEdits(task)"
+      />
+      <a @click="saveEdits(task)" title="save changes"
+        ><i class="material-icons">done</i></a
+      >
+      <a @click="currentlyEditing = false" title="undo changes"
+        ><i class="material-icons">undo</i></a
+      >
+    </div>
   </div>
 </template>
 
@@ -19,6 +38,11 @@ export default {
   props: {
     task: Object
   },
+  data() {
+    return {
+      currentlyEditing: false
+    };
+  },
   methods: {
     async toggleTaskComplete(task) {
       if (task.completed) {
@@ -28,6 +52,11 @@ export default {
         task.completed = new Date().getDate();
         await store.dispatch('updateTask', task);
       }
+    },
+
+    async saveEdits(task) {
+      await store.dispatch('updateTask', task);
+      this.currentlyEditing = false;
     }
   }
 };
