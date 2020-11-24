a9953ddd0cc352c552289b7684ba9cbfc3ca4140
John Knotts <jhnnyk@gmail.com>
2020-11-24 08:21:25 -0700

    start on collaborators - loading state

--- src/components/page-settings/PageCollaboration.vue
+++ src/components/page-settings/PageCollaboration.vue
@@ -1,11 +1,41 @@
 <template>
   <div class="page-collaboration">
     Collaborators:
+
+    <input
+      type="text"
+      placeholder="Enter email"
+      v-model="invitedUserEmail"
+      @keydown.enter="inviteCollaborator"
+    />
+    <button type="button" @click="inviteCollaborator">
+      <img v-if="loading" src="../../assets/30.svg" alt="loading" />
+      <span v-else> <i class="material-icons">person_add</i> Invite User </span>
+    </button>
   </div>
 </template>
 
 <script>
+import { firebaseFunc } from '../../firebase';
+
+const findUserByEmail = firebaseFunc.httpsCallable('findUserByEmail');
+
 export default {
-  name: 'PageCollaboration'
+  name: 'PageCollaboration',
+  data() {
+    return {
+      invitedUserEmail: '',
+      feedback: '',
+      loading: false
+    };
+  },
+  methods: {
+    async inviteCollaborator() {
+      this.loading = true;
+      const result = await findUserByEmail({ email: this.invitedUserEmail });
+      console.log(result);
+      this.loading = false;
+    }
+  }
 };
 </script>
